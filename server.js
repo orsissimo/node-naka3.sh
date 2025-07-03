860be80a12616a9ca1287d30307fc98261d558a0
Simone Orsi <simone@florasolutions.it>
2025-07-03 08:31:54 -0400

    Rename minor things

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { spawn } = require('child_process');
-const path = require('path');
-const cors = require('cors');
-const Convert = require('ansi-to-html');
+const express = require("express");
+const { spawn } = require("child_process");
+const path = require("path");
+const cors = require("cors");
+const Convert = require("ansi-to-html");
 
 const app = express();
 const PORT = 3000;
@@ -14,82 +14,78 @@ app.use(express.static(__dirname));
 
 let monitorProcess = null;
 
-app.post('/execute', (req, res) => {
-    const { command, type } = req.body;
-    
-    if (!command || !type) {
-        return res.status(400).json({ error: 'Command and type are required' });
+app.post("/execute", (req, res) => {
+  const { command, type } = req.body;
+
+  if (!command || !type) {
+    return res.status(400).json({ error: "Command and type are required" });
+  }
+
+  const [cmd, ...args] = command.split(" ");
+
+  console.log(`Executing: ${command}`);
+
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Transfer-Encoding": "chunked",
+  });
+
+  const process = spawn(cmd, args, {
+    cwd: __dirname,
+    stdio: ["pipe", "pipe", "pipe"],
+  });
+
+  if (type === "monitor") {
+    monitorProcess = process;
+  }
+
+  process.stdout.on("data", (data) => {
+    const htmlOutput = convert.toHtml(data.toString());
+    res.write(htmlOutput);
+  });
+
+  process.stderr.on("data", (data) => {
+    const htmlOutput = convert.toHtml(data.toString());
+    res.write(htmlOutput);
+  });
+
+  process.on("close", (code) => {
+    res.write(`\nProcess exited with code ${code}\n`);
+    res.end();
+
+    if (type === "monitor") {
+      monitorProcess = null;
     }
+  });
 
-    const [cmd, ...args] = command.split(' ');
-    
-    console.log(`Executing: ${command}`);
-    
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-        'Transfer-Encoding': 'chunked'
-    });
+  process.on("error", (error) => {
+    res.write(`Error: ${error.message}\n`);
+    res.end();
 
-    const process = spawn(cmd, args, {
-        cwd: __dirname,
-        stdio: ['pipe', 'pipe', 'pipe']
-    });
-
-    if (type === 'monitor') {
-        monitorProcess = process;
+    if (type === "monitor") {
+      monitorProcess = null;
     }
-
-    process.stdout.on('data', (data) => {
-        const htmlOutput = convert.toHtml(data.toString());
-        res.write(htmlOutput);
-    });
-
-    process.stderr.on('data', (data) => {
-        const htmlOutput = convert.toHtml(data.toString());
-        res.write(htmlOutput);
-    });
-
-    process.on('close', (code) => {
-        res.write(`\nProcess exited with code ${code}\n`);
-        res.end();
-        
-        if (type === 'monitor') {
-            monitorProcess = null;
-        }
-    });
-
-    process.on('error', (error) => {
-        res.write(`Error: ${error.message}\n`);
-        res.end();
-        
-        if (type === 'monitor') {
-            monitorProcess = null;
-        }
-    });
+  });
 });
 
-app.post('/stop-monitor', (req, res) => {
-    if (monitorProcess) {
-        monitorProcess.kill('SIGTERM');
-        monitorProcess = null;
-        res.json({ message: 'Monitor process stopped' });
-    } else {
-        res.json({ message: 'No monitor process running' });
-    }
+app.post("/stop-monitor", (req, res) => {
+  if (monitorProcess) {
+    monitorProcess.kill("SIGTERM");
+    monitorProcess = null;
+    res.json({ message: "Monitor process stopped" });
+  } else {
+    res.json({ message: "No monitor process running" });
+  }
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/advanced', (req, res) => {
-    res.sendFile(path.join(__dirname, 'advanced.html'));
+app.get("/advanced", (req, res) => {
+  res.sendFile(path.join(__dirname, "advanced.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Icarus Sandbox server running on http://localhost:${PORT}`);
-    console.log('Available scripts:');
-    console.log('- create-transaction.sh [miner1|miner2|miner3]');
-    console.log('- deploy-contract.sh [miner1|miner2|miner3]');
-    console.log('- monitor-chain.sh [miner1|miner2|miner3]');
+  console.log(`Stacks Playground server running on http://localhost:${PORT}`);
 });
